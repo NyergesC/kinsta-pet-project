4d2a89b9f7e353371772a17b72359932a00f4636
NyergesC <cintia.nyerges@gmail.com>
2022-08-25 14:22:55 +0200

    isLoading added

--- src/components/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs.tsx
@@ -4,20 +4,25 @@ import BlogList from './BlogList'
 
 const Blogs: React.FC<{}>= () => {
   const [blogs, setBlogs] = useState(null)
+  const [isPending, SetIsPending] = useState(true)
 
   useEffect(() => {
-      fetch("http://localhost:3001/blogs")
+    setTimeout(() => {
+        fetch("http://localhost:3001/blogs")
         .then(res => {
             return res.json()
         })
         .then ((data) =>{
             setBlogs(data)
+            SetIsPending(false)
         })
+    }, 1000)
   }, [])
 
 
   return (
     <div>
+        { isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs={blogs} />}
     </div>
   )
