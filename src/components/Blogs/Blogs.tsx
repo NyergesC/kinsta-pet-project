c3caae1add5cae69b8acd2f8c917a62bc1e5d0a3
NyergesC <cintia.nyerges@gmail.com>
2022-08-25 14:35:32 +0200

    error messages

--- src/components/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs.tsx
@@ -5,25 +5,35 @@ import BlogList from './BlogList'
 const Blogs: React.FC<{}>= () => {
   const [blogs, setBlogs] = useState(null)
   const [isPending, SetIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     setTimeout(() => {
         fetch("http://localhost:3001/blogs")
-        .then(res => {
-            return res.json()
-        })
-        .then ((data) =>{
-            setBlogs(data)
-            SetIsPending(false)
-        })
+            .then(res => {
+                if(!res.ok){
+                    throw Error('could not fetch the data for that resource')
+                }
+                return res.json()
+            })
+            .then ((data) =>{
+                setBlogs(data)
+                SetIsPending(false)
+                setError(null)
+            })
+            .catch(err=>{
+                SetIsPending(false)
+                setError(err.message)
+            })
     }, 1000)
   }, [])
 
 
   return (
     <div>
+        { error && <div>{error}</div>}
         { isPending && <div>Loading...</div>}
-        {blogs && <BlogList blogs={blogs} />}
+        { blogs && <BlogList blogs={blogs} />}
     </div>
   )
 }
