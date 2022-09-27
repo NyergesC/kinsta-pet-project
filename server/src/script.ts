6aa9c3709fcbcc1c4b3ec021293db8f8a8301b21
NyergesC <cintia.nyerges@gmail.com>
2022-09-27 13:31:59 +0200

    schema prisma is done

--- server/src/script.ts
+++ server/src/script.ts
@@ -1,12 +1,13 @@
 // 1
-const { PrismaClient } = require("@prisma/client")
+ const { PrismaClient } = require("@prisma/client") 
+/* import { PrismaClient } from "@prisma/client" */
 
 // 2
 const prisma = new PrismaClient()
 
 // 3
 async function main() {
-const newTrip= await prisma.trip.create({
+const newTrip = await prisma.trip.create({
     data: {
         destination:"Pluto",
         description:"it has a big heart",
@@ -18,12 +19,16 @@ const newTrip= await prisma.trip.create({
     })
   const allTrips = await prisma.trip.findMany()
   console.log(allTrips)
+
+  const users = await prisma.user.findMany()
+  
+
 }
 
 // 4
 main()
   .catch(e => {
-    throw e
+    console.log(e.message)
   })
 // 5
   .finally(async () => {
