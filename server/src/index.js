af02a67f7b3ce421ec60c2b4854844f192ec0d9b
NyergesC <cintia.nyerges@gmail.com>
2022-09-28 12:34:14 +0200

    schema.graphl setup async with prisma.schema

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,6 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(
       path.join(__dirname, 'schema.graphql'),
