aa39cc00dfaef4212adecad7a885bf0c83f095a9
NyergesC <cintia.nyerges@gmail.com>
2022-10-19 15:45:16 +0200

    cleanup

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -5,7 +5,7 @@ import {NavBar, NavbarContainer, NavItems, NavLogo, MobileIcon, NavMenu, NavItem
 import * as data from './links.json'
 import { useGlobalContext } from "../../UserContext";
 import {LinkR, Links} from './Types'
-import {Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { AUTH_TOKEN } from '../../constants';
 
 const linksString = JSON.stringify(data)
@@ -43,13 +43,10 @@ const Nav: React.FC<{}> = () => {
                     </MobileIcon>
                     <LinkItem links={links} /> 
 
-{/*                     <NavBtn>{authToken && <NavBtnLink to="/signin"> Login </NavBtnLink>}
- */}
                     <NavBtn color='white'>{authToken ? <NavBtnLink to="/" onClick={() => { localStorage.removeItem(AUTH_TOKEN);  navigate(`/`); }} color='white' >Logout </NavBtnLink>  : (
                     <NavBtnLink to="/signin" color='white' > Login  </NavBtnLink> )}
                    </NavBtn>
-{/*                   </NavBtn>
- */}
+
                 </NavItems>
             </NavbarContainer>
             <UserDiv>
@@ -62,60 +59,3 @@ const Nav: React.FC<{}> = () => {
 } 
 
 export default Nav 
-/* 
-import {FaBars} from 'react-icons/fa'
-import  {IconContext} from 'react-icons'
-import {NavBar, NavbarContainer, NavItems, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink, UserDiv, OutButton, LogoutOutlinedS} from './NavElements'
-import * as data from './links.json'
-import { useGlobalContext } from "../../UserContext";
-import {Link, Links} from './Types'
-
-const linksString = JSON.stringify(data)
-const links = JSON.parse(linksString).links
-
-const LinkItem: React.FC<Links> = ( { links }) => {
-    return (   
-        <NavMenu>{links.map((link: Link) => {
-            return(
-                <NavItem key={link.href}>
-                    <NavLinks to={link.href}>{link.label}</NavLinks>
-                </NavItem>
-            )
-        })}
-        </NavMenu>
- 
-    )
-};
-
-const Nav: React.FC<{}> = () => {
-
-
-    const {user, setUser} = useGlobalContext()
-    const guestUser = 'Guest'
-
-  return (
-    <IconContext.Provider value={{color:'#fff'}}>
-        <NavBar>
-            <NavbarContainer>
-                <NavLogo to="/">Logo</NavLogo> 
-                <NavItems>
-                    <MobileIcon >
-                        <FaBars />
-                    </MobileIcon>
-                    <LinkItem links={links} /> 
-                    <NavBtn>
-                        <NavBtnLink to='/signin'>Login</NavBtnLink>
-                    </NavBtn>
-                </NavItems>
-            </NavbarContainer>
-            <UserDiv>
-                <h3>Hello {user}!</h3>
-                {user === guestUser! || <OutButton onClick={() => setUser('Guest')}><LogoutOutlinedS/></OutButton>}
-            </UserDiv> 
-        </NavBar>
-    </IconContext.Provider>
-  )
-}
-
-export default Nav 
- */
